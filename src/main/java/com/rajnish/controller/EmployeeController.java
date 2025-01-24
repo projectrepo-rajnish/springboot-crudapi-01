15a5d1a4c6ff1d9f277a8f1c648f3c7ea7e35025
projectrepo-rajnish <backendcode.front@gmail.com>
2025-01-24 14:01:55 +0530

    add new changes

--- src/main/java/com/rajnish/controller/EmployeeController.java
+++ src/main/java/com/rajnish/controller/EmployeeController.java
@@ -1,9 +1,12 @@
 package com.rajnish.controller;
 
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -31,5 +34,12 @@ public class EmployeeController {
 		EmployeeDTO createEmployee = this.service.createEmployee(employee);
 		
 		return new ResponseEntity<>(createEmployee,HttpStatus.OK);
+	}
+	
+	@GetMapping
+	public ResponseEntity<List<EmployeeDTO>> getAllEmployees(){
+		
+		List<EmployeeDTO> allEmployees = this.service.getAllEmployees();
+		return new ResponseEntity<List<EmployeeDTO>>(allEmployees, HttpStatus.OK);
 	}
 }
---
+++ src/main/java/com/rajnish/exception/SampleClass.java
@@ -0,0 +1,9 @@
+package com.rajnish.exception;
+
+public class SampleClass {
+
+	public static void main(String args[]){
+		// TODO Auto-generated method stub
+System.out.println("Hello");
+	}
+}
